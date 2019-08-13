bc1f26b25d9d33b3503d7a204d305cd57daab66c
YRD <1127199932@qq.com>
2019-08-13 22:35:38 +0800

    添加交易记录、调仓记录及消息之entity By 杨日东

---
+++ financial_management/src/main/java/financial_management/entity/Message.java
@@ -0,0 +1,82 @@
+package financial_management.entity;
+
+import java.sql.Date;
+
+public class Message {
+    //消息ID
+    private Long ID;
+
+    //消息发送时间
+    private Date time;
+
+    //消息接收用户ID
+    private Integer userID;
+
+    //消息类型
+    private Integer type;
+
+    //消息内容
+    private String content;
+
+    //消息是否已读
+    private Boolean isRead;
+
+    //消息是否被标识为删除
+    private Boolean isDelete;
+
+    public Long getID() {
+        return ID;
+    }
+
+    public void setID(Long iD) {
+        ID = iD;
+    }
+
+    public Date getTime() {
+        return time;
+    }
+
+    public void setTime(Date time) {
+        this.time = time;
+    }
+
+    public Integer getUserID() {
+        return userID;
+    }
+
+    public void setUserID(Integer userID) {
+        this.userID = userID;
+    }
+
+    public Integer getType() {
+        return type;
+    }
+
+    public void setType(Integer type) {
+        this.type = type;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Boolean getIsRead() {
+        return isRead;
+    }
+
+    public void setIsRead(Boolean isRead) {
+        this.isRead = isRead;
+    }
+
+    public Boolean getIsDelete() {
+        return isDelete;
+    }
+
+    public void setIsDelete(Boolean isDelete) {
+        this.isDelete = isDelete;
+    }
+}
---
+++ financial_management/src/main/java/financial_management/entity/TransferRecord.java
@@ -0,0 +1,93 @@
+package financial_management.entity;
+
+import java.sql.Date;
+
+public class TransferRecord{
+    //调仓记录ID
+    private Long ID;
+
+    //调仓创建时间
+    private Date createTime;
+
+    //调仓完成时间
+    private Date completeTime;
+
+    //减持类型
+    private Integer sellType;
+
+    //增持类型
+    private Integer buyType;
+
+    //调仓用户ID
+    private Integer userID;
+
+    //调仓状态
+    private Integer status;
+
+    //是否为用户手动调仓
+    private Boolean isCustomize;
+
+    public Long getID() {
+        return ID;
+    }
+
+    public void setID(Long iD) {
+        ID = iD;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getCompleteTime() {
+        return completeTime;
+    }
+
+    public void setCompleteTime(Date completeTime) {
+        this.completeTime = completeTime;
+    }
+
+    public Integer getSellType() {
+        return sellType;
+    }
+
+    public void setSellType(Integer sellType) {
+        this.sellType = sellType;
+    }
+
+    public Integer getBuyType() {
+        return buyType;
+    }
+
+    public void setBuyType(Integer buyType) {
+        this.buyType = buyType;
+    }
+
+    public Integer getUserID() {
+        return userID;
+    }
+
+    public void setUserID(Integer userID) {
+        this.userID = userID;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public Boolean getIsCustomize() {
+        return isCustomize;
+    }
+
+    public void setIsCustomize(Boolean isCustomize) {
+        this.isCustomize = isCustomize;
+    }
+}

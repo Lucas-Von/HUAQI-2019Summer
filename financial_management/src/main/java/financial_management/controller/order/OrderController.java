32b9d0d49982a8453b9eef62b051cf9e59c23658
YRD <1127199932@qq.com>
2019-08-19 17:38:12 +0800

    对orderController的一些修改 by 杨日东

--- financial_management/src/main/java/financial_management/controller/order/OrderController.java
+++ financial_management/src/main/java/financial_management/controller/order/OrderController.java
@@ -19,36 +19,28 @@ public class OrderController {
     @GetMapping("trade")
     public ResponseEntity<?> getTradeRecordsByUserID(@RequestParam Long userID){
         Response<List<TradeRecordVO>> response = orderService.getTradeRecordByUser(userID);
-        if (response.isSuccess()){
-            return ResponseEntity.ok(response.getContent());
-        } else {
-            return ResponseEntity.status(response.getStatusCode()).body(response.getMessage());
-        }
+        return judge(response);
     }
 
     @GetMapping("trade/{ID}")
     public ResponseEntity<?> getTradeRecordByRecordID(@PathVariable Long ID){
         Response<TradeRecordVO> response = orderService.getTradeRecordByRecord(ID);
-        if (response.isSuccess()){
-            return ResponseEntity.ok(response.getContent());
-        } else {
-            return ResponseEntity.status(response.getStatusCode()).body(response.getMessage());
-        }
+        return judge(response);
     }
 
     @GetMapping("transfer")
     public ResponseEntity<?> getTransferRecordByUserID(@RequestParam Long userID){
         Response<List<TransferRecordVO>> response = orderService.getTransferRecordByUser(userID);
-        if (response.isSuccess()){
-            return ResponseEntity.ok(response.getContent());
-        } else {
-            return ResponseEntity.status(response.getStatusCode()).body(response.getMessage());
-        }
+        return judge(response);
     }
 
     @GetMapping("/transfer/{ID}")
     public ResponseEntity<?> getTransferRecordByRecordID(@PathVariable Long ID){
         Response<TransferRecordVO> response = orderService.getTransferRecordByRecord(ID);
+        return judge(response);
+    }
+
+    private static ResponseEntity judge(Response response){
         if (response.isSuccess()){
             return ResponseEntity.ok(response.getContent());
         } else {

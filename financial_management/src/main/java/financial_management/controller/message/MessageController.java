c2812e41284b0d471c67c0a74442bcd2def5dc6c
YRD <1127199932@qq.com>
2019-08-19 22:13:48 +0800

    message模块的controller，vo和stub by 杨日东

---
+++ financial_management/src/main/java/financial_management/controller/message/MessageController.java
@@ -0,0 +1,62 @@
+package financial_management.controller.message;
+
+import financial_management.bl.message.MessageService;
+import financial_management.entity.Response;
+import financial_management.vo.message.MessageVO;
+import financial_management.vo.message.NewMessageVO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/message/")
+public class MessageController {
+    @Autowired
+    private MessageService messageService;
+
+    @GetMapping("get")
+    public ResponseEntity<?> getMessagesByUser(@RequestParam Long ID) {
+        Response<List<MessageVO>> response = messageService.getMessagesByUser(ID);
+        return judge(response);
+    }
+
+    @GetMapping("get")
+    public ResponseEntity<?> getMessagesByUser(@RequestParam Long ID, @RequestParam int type) {
+        Response<List<MessageVO>> response = messageService.getMessagesByUser(ID, type);
+        return judge(response);
+    }
+
+    @GetMapping("get")
+    public ResponseEntity<?> getMessagesByUser(@RequestParam Long ID, @RequestParam int type, @RequestParam int page) {
+        Response<List<MessageVO>> response = messageService.getMessagesByUser(ID, type, page);
+        return judge(response);
+    }
+
+    @GetMapping("getNew")
+    public ResponseEntity<?> getNewMessageByUser(@RequestParam Long ID){
+        Response<List<NewMessageVO>> response = messageService.getNewMessageByUser(ID);
+        return judge(response);
+    }
+
+    @PostMapping("hasRead")
+    public ResponseEntity<?> readNewMessage(@RequestParam Long userID,@RequestParam int type){
+        Response<?> response = messageService.readNewMessages(userID,type);
+        return judge(response);
+    }
+
+    @DeleteMapping("del/{ID}")
+    public ResponseEntity<?> delMessageByMessageID(@PathVariable Long ID){
+        Response<?> response = messageService.removeMessageByMessageID(ID);
+        return judge(response);
+    }
+
+    private static ResponseEntity judge(Response response) {
+        if (response.isSuccess()) {
+            return ResponseEntity.ok(response.getContent());
+        } else {
+            return ResponseEntity.status(response.getStatusCode()).body(response.getMessage());
+        }
+    }
+}
---
+++ financial_management/src/main/java/financial_management/vo/message/NewMessageVO.java
@@ -0,0 +1,37 @@
+package financial_management.vo.message;
+
+public class NewMessageVO {
+    private int type;
+    private String latest;
+    private int unreadAmount;
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public String getLatest() {
+        return latest;
+    }
+
+    public void setLatest(String latest) {
+        this.latest = latest;
+    }
+
+    public int getUnreadAmount() {
+        return unreadAmount;
+    }
+
+    public void setUnreadAmount(int unreadAmount) {
+        this.unreadAmount = unreadAmount;
+    }
+
+    public NewMessageVO(int type, String latest, int unreadAmount) {
+        this.type = type;
+        this.latest = latest;
+        this.unreadAmount = unreadAmount;
+    }
+}

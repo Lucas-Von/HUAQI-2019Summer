ddd8da5cee7494c186d334654cdcadadf72c266c
xyh <467946553@qq.com>
2019-08-29 22:53:11 +0800

    feat:测试调用python接口

---
+++ financial_management/src/main/java/financial_management/util/PyInvoke/PyResponse/GoldConfigResponse.java
@@ -0,0 +1,79 @@
+package financial_management.util.PyInvoke.PyResponse;
+
+/**
+ * @author xyh
+ * @date 2019/8/29
+ */
+public class GoldConfigResponse {
+    private double price2deploy;
+    private int account2deployed;
+    private double money2deployed;
+    private int accounet_all_deployed;
+    private double money_all_deployed;
+    private double diff_all_deployed;
+
+    public double getPrice2deploy() {
+        return price2deploy;
+    }
+
+    public void setPrice2deploy(double price2deploy) {
+        this.price2deploy = price2deploy;
+    }
+
+    public int getAccount2deployed() {
+        return account2deployed;
+    }
+
+    public void setAccount2deployed(int account2deployed) {
+        this.account2deployed = account2deployed;
+    }
+
+    public double getMoney2deployed() {
+        return money2deployed;
+    }
+
+    public void setMoney2deployed(double money2deployed) {
+        this.money2deployed = money2deployed;
+    }
+
+    public int getAccounet_all_deployed() {
+        return accounet_all_deployed;
+    }
+
+    public void setAccounet_all_deployed(int accounet_all_deployed) {
+        this.accounet_all_deployed = accounet_all_deployed;
+    }
+
+    public double getMoney_all_deployed() {
+        return money_all_deployed;
+    }
+
+    public void setMoney_all_deployed(double money_all_deployed) {
+        this.money_all_deployed = money_all_deployed;
+    }
+
+    public double getDiff_all_deployed() {
+        return diff_all_deployed;
+    }
+
+    public void setDiff_all_deployed(double diff_all_deployed) {
+        this.diff_all_deployed = diff_all_deployed;
+    }
+
+    public GoldConfigResponse() {
+    }
+
+    public GoldConfigResponse(double price2deploy,
+                              int account2deployed,
+                              double money2deployed,
+                              int accounet_all_deployed,
+                              double money_all_deployed,
+                              double diff_all_deployed) {
+        this.price2deploy = price2deploy;
+        this.account2deployed = account2deployed;
+        this.money2deployed = money2deployed;
+        this.accounet_all_deployed = accounet_all_deployed;
+        this.money_all_deployed = money_all_deployed;
+        this.diff_all_deployed = diff_all_deployed;
+    }
+}

a87f4e3aa97408b090cace4e1ec023be155ad9cd
jyh <2433495058@qq.com>
2019-09-08 13:41:32 +0800

    faet:保险接口和现金接口

--- financial_management/src/main/java/financial_management/entity/fund/InfoParam.java
+++ financial_management/src/main/java/financial_management/entity/fund/InfoParam.java
@@ -1,4 +1,22 @@
 package financial_management.entity.fund;
 
 public class InfoParam {
+    Float accumulated_earning;
+    Float balance;
+
+    public Float getAccumulated_earning() {
+        return accumulated_earning;
+    }
+
+    public void setAccumulated_earning(Float accumulated_earning) {
+        this.accumulated_earning = accumulated_earning;
+    }
+
+    public Float getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Float balance) {
+        this.balance = balance;
+    }
 }
